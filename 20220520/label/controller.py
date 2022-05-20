89fa6ac1a79111d3929587806d60817a9ba65b46
t102458054 <t102458054@outlook.com>
2022-05-20 23:18:46 +0800

    20220520

---
+++ 20220520/label/controller.py
@@ -0,0 +1,14 @@
+from PyQt5 import QtWidgets, QtGui, QtCore
+from ui import Ui_MainWindow
+
+
+class MainWindow_controller(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.setup_control()
+
+    def setup_control(self):
+        # TODO
+        self.ui.label.setText('Happy World!')
